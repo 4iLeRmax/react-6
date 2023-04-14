87d6068bb1cfb561016125c0ff68115b7850330c
4iLeRmax <max.zlatin@gmail.com>
2023-04-14 15:13:17 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,49 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import json from './data.json';
+
+import Collection from './Collection';
+
 import './index.scss';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+const cats = [
+  { "name": "Все" },
+  { "name": "Море" },
+  { "name": "Горы" },
+  { "name": "Архитектура" },
+  { "name": "Города" }
+];
 
 function App() {
+  const [categoryId, setCategoryId] = useState(0);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [colections, setColections] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    setColections(json.collections);
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000);
+  }, [categoryId]);
+
+  useEffect(()=>{
+    
+  }, [page]);
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {cats.map((cat, index) =>
+            <li
+              key={cat.name}
+              className={categoryId === index ? 'active' : ''}
+              onClick={() => setCategoryId(index)}
+            >{cat.name}</li>
+          )}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">
+        {isLoading ? (
+          <div className="loading">
+            <h2>Loading<span className="dot-1">.</span><span className="dot-2">.</span><span className="dot-3">.</span></h2>
+          </div>
+        ) : (
+          colections
+          .filter(item =>
+            (item.name).toLowerCase().includes(searchValue.toLowerCase())
+          )
+          .filter(item => {
+            if (item.category === categoryId) return true;
+            if (categoryId === 0) return true;
+          })
+          .map((item, index) =>
+            <Collection
+              key={index}
+              name={item.name}
+              images={item.photos}
+            />
+          )
+        )}
+      </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+          {
+            [...Array(5)].map((_, index)=> (
+              <li 
+                key={index}
+                className={page === index + 1 ? 'active' : ''}
+                onClick={()=> setPage(index + 1)}
+              >{index+1}</li>
+            ))
+          }
       </ul>
     </div>
   );
